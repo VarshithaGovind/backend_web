39353d5e8a0fad5acce79c31999ca8d2fefe35fa
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-07-02 18:43:19 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-const { connectV } = require('../config/db');
-const conn = connectV();
 
 const bookingSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    project: { type: String, required: true },
-    date: { type: Date, default: Date.now },
-    status: { type: String, default: 'pending' }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  project: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  status: { type: String, default: 'pending' }
 });
 
-module.exports = conn.model('Booking', bookingSchema);
+module.exports = mongoose.model('Booking', bookingSchema);
