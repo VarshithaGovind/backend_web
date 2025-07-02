2e2bddfa243d95508fb73f33ad4c8e016ea9c82f
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-07-02 19:34:19 +0530

    Update majorProject.js

--- models/majorProject.js
+++ models/majorProject.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const majorProjectSchema = new mongoose.Schema({
   title: String,
   description: String,
+  tech: String,
+  duration: String,
   image: String,
+  trainer: Boolean,
   languages: [String],
   guideSteps: [String],
-  clubOnly: Boolean,
 });
 
 module.exports = mongoose.model('MajorProject', majorProjectSchema);
