8bd20063cbbf85a209ec23684172428b0fdfad9a
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-07-02 18:36:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const connectDB = require('./db');
+const connectDB = require('./config/db');
 connectDB();
 
 // ✅ More permissive CORS for development/testing
